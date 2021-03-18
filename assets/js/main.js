c9f0c40350df5f33359b69703d6f38c0aef1a251
Mfigueroaa <mfigueroaa@fen.uchile.cl>
2021-03-17 23:58:48 -0300

    Orden Main JS -  Se agrega FADE OUT cambio de Página

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,15 +39,49 @@
 			});
 
 
-			// Spinner Wrapper 
+		// Spinner Wrapper 
 			let spinnerWrapper = document.querySelector('.spinner-wrapper');
 		window.addEventListener('load', function () {
 			
 			$(spinnerWrapper).fadeOut("slow");
 		});
 
+		// Scroll 
 
+		$(".glow-on-hover").click(function() {
+			$('html,body').animate({
+			scrollTop: $(".wrapper").offset().top},
+			'slow');
 			
+			
+			});
+
+
+		//Fade Out Function
+			
+			// delegate all clicks on "a" tag (links)
+$('#menu').on("click", "a", function () {
+
+    // get the href attribute
+    var newUrl = $(this).attr("href");
+
+    // veryfy if the new url exists or is a hash
+    if (!newUrl || newUrl[0] === "#") {
+        // set that hash
+        location.hash = newUrl;
+        return;
+    }
+    // now, fadeout the html (whole page)
+    $("html").fadeOut(function () {
+        // when the animation is complete, set the new location
+        location = newUrl;
+    });
+
+    // prevent the default browser behavior.
+    return false;
+});
+
+		
 
 })
 
