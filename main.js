f429851e61e2a8e98726eb81278abaf0681cb7e7
erizokun <iperezs@fen.uchile.cl>
2021-03-24 18:39:48 -0300

    quitar pop up al ni clickearlo listo

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ var modals = document.getElementsByClassName('modal-container'); /* acá va el d
 var btns = document.getElementsByClassName("ctamodal"); /* Acá va el div del botón o el que abre el modal */
 var btnclose = document.getElementsByClassName("close"); /* Acá va el div o botón que cierra el modal (el cual va dentro del modal) */
 
-
 for(let i=0;i<btns.length;i++){ /* El valor que precede a length es la variable abre el modal (openmodal) */
     btns[i].onclick = function() { /* Acá, para el botón de abrir "i", cuando aprete click, me ejecuta la función */
         /* modals[i].style.display = "block"; */
@@ -14,6 +13,7 @@ for(let i=0;i<btns.length;i++){ /* El valor que precede a length es la variable 
         modals[i].style.visibility = "visible";
     }
 }
+
 for(let i=0;i<btnclose.length;i++){ /* El valor que precede a length es la variable cierra el modal (close) */
     btnclose[i].onclick = function() { /* Acá, para el botón de cerrar "i", cuando aprete click, me ejecuta la función */
         /* modals[i].style.display = "none"; *//* Acá lo que se hace es que desactiva el modal "i" al hacer click en el botón "i" */
@@ -22,4 +22,18 @@ for(let i=0;i<btnclose.length;i++){ /* El valor que precede a length es la varia
     }
 }
 
+/* Cerrar modal click en cualquier parte */
+for(let i=0;i<modals.length;i++){
+window.addEventListener("click",function(e){
+    console.log(e.target)
+    if(e.target == modals[i]){
+        ;
+    
+        setTimeout(function() {
+            modals[i].style.opacity = "0"; 
+            modals[i].style.visibility = "hidden"; 
+        },450)
+    }
+})
+}
 /* ssssssssssssssssssssssssssssssssssssssss */
